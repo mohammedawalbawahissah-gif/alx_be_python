56f7cacec6f8f33a2443102ce3600cf1c3ab04d5
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2025-11-05 10:54:44 +0000

    git commit -m "basic_operations"

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,2 +1,8 @@
 number1 = 10
 number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+print(sum_result)
+print(difference_result)
+print(product_result)
