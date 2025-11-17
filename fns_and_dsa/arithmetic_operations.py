a3d8fde81ec06ad450d538caee6b38f34cbd7c8e
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2025-11-17 19:17:07 +0000

    git commit -m "arithmetic_operations.py"

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Perform basic arithmetic operations based on the operation parameter.
+
+    Parameters:
+    -num1 (float): The first number
+    -num2 (float): The second number
+    -operation (str): The operation to perform: 'add', 'subtract', 'multiply', 'divide'
+
+    Returns:
+    - float: Result of the arithmetic operation
+    - str: Error message in case of division by zero or invalid operation
+    """
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1/num2
+    else:
+        return "Error: Invalid operation. Choose add, subtract, multiply, or divide"
